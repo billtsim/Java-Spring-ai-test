1288b276d27dd5aaca50963c1fc1c6dfddf9d1d4
billtsim <bill85504@gamil.com>
2024-05-22 11:46:10 +0800

    ai-test

---
+++ src/main/java/com/a88/JavaSpringAiTestApplication.java
@@ -0,0 +1,13 @@
+package com.a88;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringAiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringAiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/a88/JavaSpringAiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a88;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringAiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
